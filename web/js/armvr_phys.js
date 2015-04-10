d3639ff44df2e10ff1ea41f5f7c9b708513031d5
PyryM <pkmatikainen@gmail.com>
2015-04-09 22:31:03 -0400

    urgg mostly working now on new mozvr

--- web/js/armvr_phys.js
+++ web/js/armvr_phys.js
@@ -1,6 +1,8 @@
 var camera, scene, renderer;
+var controls, effect;
 var mesh;
 var stats;
+var basescene;
 
 var windowX, windowY;
 var windowHalfX, windowHalfY;
@@ -14,9 +16,8 @@ var opened = false;
 var lefthand;
 var righthand;
 var handsopen = [false, false];
-
-var deviceManager;
-var riftSandbox;
+var camtarget;
+var fakeobject = {"quat": [0,0,0,1], "pos": [0,0,0]};
 
 var boxtray = null;
 
@@ -56,9 +57,13 @@ function toggleVrMode() {
 
 
 function onKeyDown(event) {
+	event.preventDefault();
+
 	if(event.keyCode == 70) { // f
 		console.log("Trying to toggle VR mode...");
-		userToggleVRMode();
+		effect.setFullScreen( true );
+	} else if (event.keyCode == 90) { // z
+	    controls.zeroSensor();
 	} else {
 		console.log("Some other key: " + event.keyCode);
 	}
@@ -67,42 +72,32 @@ function onKeyDown(event) {
 
 function init() {
 
-	deviceManager = new DeviceManager();
-	riftSandbox = new RiftSandbox(window.innerWidth, window.innerHeight);
-
-    deviceManager.onResizeFOV = function (renderTargetSize, fovLeft, fovRight) {
-     	riftSandbox.setFOV(fovLeft, fovRight);
-    }
-
-    deviceManager.onHMDDeviceFound = function (hmdDevice) {
-    	var eyeOffsetLeft = hmdDevice.getEyeTranslation("left");
-    	var eyeOffsetRight = hmdDevice.getEyeTranslation("right");
-    	riftSandbox.setCameraOffsets(eyeOffsetLeft, eyeOffsetRight);
-    }
-
-    document.addEventListener('mozfullscreenchange', toggleVrMode, false);
-    document.addEventListener('webkitfullscreenchange', toggleVrMode, false);
     document.addEventListener('keydown', onKeyDown);
 
-    deviceManager.init();
-
-	// renderer = new THREE.WebGLRenderer({antialias: true});
-	// renderer.setSize( window.innerWidth, window.innerHeight );
-	// document.body.appendChild( renderer.domElement );
+	renderer = new THREE.WebGLRenderer({antialias: true});
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	document.body.appendChild( renderer.domElement );
 
 	stats = new Stats();
 	stats.domElement.style.position = 'absolute';
 	stats.domElement.style.top = '0px';
 	document.body.appendChild( stats.domElement );
 
+	camtarget = new THREE.Object3D();
+
 	getSize();
 
 	//
 
 	camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 1000 );
-	camera.position.z = 1.0;
+	//camera.position.z = 1.0;
 
-	var basescene = riftSandbox.scene; //new THREE.Scene();
+	controls = new THREE.VRControls( camera, fakeobject );
+
+	effect = new THREE.VREffect( renderer );
+	effect.setSize( window.innerWidth, window.innerHeight );
+
+	basescene = new THREE.Scene();
 	scene = new THREE.Object3D();
 	basescene.add(scene);
 	scene.position.z = -1.1;
@@ -126,31 +121,31 @@ function init() {
 	pointLight.position.set(1, 0.4, -1);
 	scene.add(pointLight);
 
-	window.addEventListener( 'resize', onWindowResize, false );
+	//window.addEventListener( 'resize', onWindowResize, false );
 
 	thegrid = new BaseGrid(60.0, 60.0, 1.0);
 	thegrid.addToScene(scene);
 
 	//initOrbitCamera(camera);
 
-	$.get("atlas.json", function(data) {
-		console.log("Got data... trying to build armature...");
-		var jdata = JSON.parse(data);
-		thearmature = new Armature(scene, jdata, "utorso");
-		thearmature.root.rotation.x = -Math.PI / 2.0;
-		thearmature.root.position.y = 0.8;
+	// $.get("atlas.json", function(data) {
+	// 	console.log("Got data... trying to build armature...");
+	// 	var jdata = JSON.parse(data);
+	// 	thearmature = new Armature(scene, jdata, "utorso");
+	// 	thearmature.root.rotation.x = -Math.PI / 2.0;
+	// 	thearmature.root.position.y = 0.8;
 
-		lefthand = new THREE.Object3D();
-		lefthand.position.set(0,0.1,0);
-		righthand = new THREE.Object3D();
-		righthand.position.set(0,-0.1,0);
-		thearmature.links.r_hand.knode.add(righthand);
-		thearmature.links.l_hand.knode.add(lefthand);
-	});
+	// 	lefthand = new THREE.Object3D();
+	// 	lefthand.position.set(0,0.1,0);
+	// 	righthand = new THREE.Object3D();
+	// 	righthand.position.set(0,-0.1,0);
+	// 	thearmature.links.r_hand.knode.add(righthand);
+	// 	thearmature.links.l_hand.knode.add(lefthand);
+	// });
 
 	animate();
 
-	init_ws();
+	//init_ws();
 
 }
 
@@ -160,28 +155,41 @@ function onWindowResize() {
 	// camera.updateProjectionMatrix();
 
 	// renderer.setSize( window.innerWidth, window.innerHeight );
-	riftSandbox.resize();
+	//riftSandbox.resize();
+	effect.setSize( window.innerWidth , window.innerHeight );
 
 }
 
 function animate() {
 
 	t += 1.0 / 60.0;
+	//console.log(">>>>>>>>>> Enter animate. >>>>>>>>>>>");
 
 	// if(thearmature) {
 	// 	thearmature.setJointRotation("l_arm_elx", Math.cos(t));
 	// 	thearmature.setJointRotation("l_arm_usy", t);
 	// }
 
-	requestAnimationFrame( animate );
-
 	// updateCamera();
 
 	// //composer.render();
 	// renderer.render(scene, camera);
+	controls.update();
+	// console.log("In animate:");
+	// console.log(fakeobject.quat);
+	// console.log(fakeobject.pos);
+	// console.log("End animate.");
+	//console.log(camera.quaternion);
+	//camera.quaternion.set(0.5, 0.5, 0.5 , 0.5);
+	//camera.quaternion.copy(camtarget.quaternion);
 	mainLoop();
+	effect.render( basescene, camera );
 
 	stats.update();
+
+	requestAnimationFrame( animate );
+
+	//console.log("<<<<<<<<<<<<< Exit animate. <<<<<<<<<<<<");
 
 }
 
@@ -223,20 +231,6 @@ function mainLoop() {
 	if(boxtray) {
 		boxtray.update();
 	}
-
-	// Apply movement
-	if (deviceManager.sensorDevice) {
-		if (riftSandbox.vrMode) {
-		 	riftSandbox.setHmdPositionRotation(deviceManager.sensorDevice.getState());
-		}
-	}
-	if (!deviceManager.sensorDevice || !riftSandbox.vrMode) {
-		// maybe update orbitcam here?
-	}
-	riftSandbox.setBaseRotation();
-	riftSandbox.updateCameraPositionRotation();
-
-	riftSandbox.render();
 
 	if(boxtray && righthand && lefthand) {
 		var vector = new THREE.Vector3();
